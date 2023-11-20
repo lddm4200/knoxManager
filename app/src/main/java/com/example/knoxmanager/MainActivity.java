0b56bc57cead675b4a54684881eb54a11ad290ea
Minh Long <longddm2k4@gmail.com>
2023-11-20 10:41:37 +0700

    this 8

--- app/src/main/java/com/example/knoxmanager/MainActivity.java
+++ app/src/main/java/com/example/knoxmanager/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private String lon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
