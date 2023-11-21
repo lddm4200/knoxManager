a16a3079a749ff43cbcebfe3f3f2059ad63989e4
Minh Long <longddm2k4@gmail.com>
2023-11-21 22:06:57 +0700

    them

--- app/src/main/java/com/example/knoxmanager/sigIn.java
+++ app/src/main/java/com/example/knoxmanager/sigIn.java
@@ -3,39 +3,34 @@ package com.example.knoxmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.io.IOException;
 
 import pl.droidsonroids.gif.GifDrawable;
 
-public class logIn extends AppCompatActivity {
-private ImageView vit;
-private TextView butumDK;
-
+public class sigIn extends AppCompatActivity {
+    private ImageView vit;
+    private ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_sig_in);
         vit=findViewById(R.id.vit);
-        butumDK=findViewById(R.id.btnDKlogin);
+        back=findViewById(R.id.backSigin);
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         try {
             GifDrawable gifDrawable = new GifDrawable(getResources(), R.drawable.img);
             vit.setImageDrawable(gifDrawable);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        butumDK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(logIn.this,sigIn.class));
-            }
-        });
     }
 }
