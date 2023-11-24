f7f9fe9d1e2bc102c95169af379c3788fe83ee95
Nam Khánh <khanhandgithub204@gmail.com>
2023-11-25 02:42:07 +0700

    Khánh nè

--- app/src/main/java/com/example/knoxmanager/frm/frmtrangChu.java
+++ app/src/main/java/com/example/knoxmanager/frm/frmtrangChu.java
@@ -20,6 +20,8 @@ import com.example.knoxmanager.model.photo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import me.relex.circleindicator.CircleIndicator;
 
@@ -30,32 +32,75 @@ public class frmtrangChu extends Fragment {
     public frmtrangChu() {
         // Required empty public constructor
     }
-private ViewPager viewPager;
+
+    private ViewPager viewPager;
     private CircleIndicator circleIndicator;
     private photoAdrapter photoAdrapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_trang_chu, container, false);
+        View view = inflater.inflate(R.layout.fragment_trang_chu, container, false);
 
-    viewPager=view.findViewById(R.id.ViewPager);
-    circleIndicator = view.findViewById(R.id.CircleIndicator);
+        viewPager = view.findViewById(R.id.ViewPager);
+        circleIndicator = view.findViewById(R.id.CircleIndicator);
 
-    photoAdrapter = new photoAdrapter(getActivity(),photoList() );
-    viewPager.setAdapter(photoAdrapter);
+        photoAdrapter = new photoAdrapter(getActivity(), photoList());
+        viewPager.setAdapter(photoAdrapter);
 
-    circleIndicator.setViewPager(viewPager);
-    photoAdrapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
+        circleIndicator.setViewPager(viewPager);
+        photoAdrapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
         return view;
     }
-    private List<photo> photoList(){
+
+
+    private List<photo> photoList() {
         List<photo> list = new ArrayList<>();
         list.add(new photo(R.drawable.baner1));
         list.add(new photo(R.drawable.baner2));
         list.add(new photo(R.drawable.baner3));
         list.add(new photo(R.drawable.baner4));
         list.add(new photo(R.drawable.baner5));
-
         return list;
+    }
+
+    private Timer autoScrollTimer;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startAutoScroll();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopAutoScroll();
+    }
+    private void startAutoScroll() {
+        autoScrollTimer = new Timer();
+        autoScrollTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // Thay đổi trang hiển thị trong ViewPager
+                if (getActivity() != null) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int currentItem = viewPager.getCurrentItem();
+                            int itemCount = viewPager.getAdapter().getCount();
+                            int nextItem = (currentItem + 1) % itemCount;
+                            viewPager.setCurrentItem(nextItem);
+                        }
+                    });
+                }
+            }
+        }, 3000, 3000);
+    }
+    private void stopAutoScroll() {
+        if (autoScrollTimer != null) {
+            autoScrollTimer.cancel();
+            autoScrollTimer = null;
+        }
     }
 }
